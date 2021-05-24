9a26f272b3b93850d7a7ab44b0edbccab554d657
Filip Yaghob <fyaghob@gmail.com>
2021-05-24 14:57:04 +0200

    Zmena formatu datumu v logovani

--- C19DataLoader/C19DataLoader/ILogger.cs
+++ C19DataLoader/C19DataLoader/ILogger.cs
@@ -8,31 +8,31 @@ namespace Fyk.C19.C19DataLoader
     {
         public void LogInformation(string message)
         {
-            Console.Out.WriteLine($"{DateTime.Now}\tINFO\t{message}");
+            Console.Out.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss}\tINFO\t{message}");
         }
         public void LogError(string message)
         {
-            Console.Error.WriteLine($"{DateTime.Now}\tERROR\t{message}");
+            Console.Error.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss}\tERROR\t{message}");
         }
 
         void IC19Logger.LogWarning(string message)
         {
-            Console.Out.WriteLine($"{DateTime.Now}\tWARN\t{message}");
+            Console.Out.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss}\tWARN\t{message}");
         }
 
         void IC19Logger.LogDebug(string message)
         {
-            Console.Out.WriteLine($"{DateTime.Now}\tDEBUG\t{message}");
+            Console.Out.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss}\tDEBUG\t{message}");
         }
 
         void IC19Logger.LogCritical(string message)
         {
-            Console.Out.WriteLine($"{DateTime.Now}\tCRITICAL\t{message}");
+            Console.Out.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss}\tCRITICAL\t{message}");
         }
 
         void IC19Logger.LogTrace(string message)
         {
-            Console.Out.WriteLine($"{DateTime.Now}\tTRACE\t{message}");
+            Console.Out.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss}\tTRACE\t{message}");
         }
     }
 }
